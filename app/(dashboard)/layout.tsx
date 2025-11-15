20cbae6f25b5988a1bd05be81ea9523f9a157128
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-11-15 07:03:14 +0600

    first commit

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TanstackProvider from "@/provider/tanstackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TanstackProvider>
+          {children}
+          <Toaster
+            position="top-center"
+            richColors
+            closeButton
+            duration={4000}
+            visibleToasts={3}
+            offset={16}
+          />
+        </TanstackProvider>
       </body>
     </html>
   );
